9e4cead0de413816fb3b73d5c9754ac21e7adcec
Himalaya Kaushik <himalya.kxa@gmail.com>
2018-10-19 13:07:36 +0530

    dates

--- newapp/views.py
+++ newapp/views.py
@@ -46,7 +46,7 @@ def set_visit(request):
 
     cur.execute(" SELECT visit_schedule FROM patient_level WHERE patient_id = %s", (patient_id,))
     records = cur.fetchall()
-    print("dates array ", records[0][0][1])
+    print("dates array ", records[0][0])
 
     #if (records[0][0] == None):
 

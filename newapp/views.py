a9e2808a3a4d34e977b452c7a0b1a3f5ca2e23f2
Himalaya Kaushik <himalya.kxa@gmail.com>
2018-10-20 18:07:11 +0530

    visit number

--- newapp/views.py
+++ newapp/views.py
@@ -1557,7 +1557,7 @@ def report_data_high_risk(request):
                 records = cur.fetchall()
 
             else:
-                cur.execute("SELECT row_to_json(patient_record) FROM (SELECT * FROM patient_level WHERE bmo_id = %s and patient_status=active) patient_record",
+                cur.execute("SELECT row_to_json(patient_record) FROM (SELECT * FROM patient_level WHERE bmo_id = %s ) patient_record",
                     (bmo_id,))
                 records = cur.fetchall()
 

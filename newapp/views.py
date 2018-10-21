07705661ef5f91ab19d364b77d513725fa14424f
Himalaya Kaushik <himalya.kxa@gmail.com>
2018-10-21 10:28:52 +0530

    dates

--- newapp/views.py
+++ newapp/views.py
@@ -1555,8 +1555,8 @@ def report_data_high_risk(request):
                 date_2 = request.GET.get('0', None)
                 date_1 = request.GET.get('1', None)
                 cur.execute(
-                    "SELECT row_to_json(patient_record) FROM (SELECT * FROM patient_level WHERE reg_date>=%s and reg_date<=%s and bmo_id = %s) patient_record",
-                    (date_2,date_1,bmo_id,))
+                    "SELECT row_to_json(patient_record) FROM (SELECT * FROM patient_level WHERE reg_date>=%s and reg_date<=%s and bmo_id = %s and patient_status=%s) patient_record",
+                    (date_2,date_1,bmo_id,"active",))
                 records = cur.fetchall()
 
             else:

3ea2cdb6e57f3410fb6e03826710cfbbb630ca4c
unknown <himalya.kxa@gmail.com>
2018-12-16 00:35:04 +0530

    anm app

--- anm_anc/views.py
+++ anm_anc/views.py
@@ -54,6 +54,7 @@ def patient_registry(request):
     res = cur.execute("""INSERT INTO patient_level (state,block,division,district,officer,agbdi_name,aadhar_number,patient_name,husband_name,husband_age,mobile_number,
                    date_of_birth, economic_status,cast_type,relegion,lmp_date,edd_date,address) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s ) returning patient_id """ , (state,block,division,district,officer,
                     agbdi_name, aadhar_number,patient_name,husband_name,husband_age,mobile_number,date_of_birth, economic_status,cast,relegion,lmp_date,edd_date,address,))
+    print(res)
     patient_id = res[0]['patient_id']
 
     return Response({'patient_id' : patient_id})

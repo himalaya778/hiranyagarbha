7f5df6a0882d6940f4ecba47de848313e9aa9f54
unknown <himalya.kxa@gmail.com>
2018-12-23 12:03:33 +0530

    smo anc

--- smo/smo_add_ons.py
+++ smo/smo_add_ons.py
@@ -3,7 +3,7 @@ from accounts.views import conn
 
 cur = conn.cursor()
 def get_visit_number(id):
-    cur.execute("SELECT visits_done FROM smo_anc WHERE patient_id = s" , (id,))
+    cur.execute("SELECT visits_done FROM smo_anc WHERE patient_id = %s" , (id,))
     records_v = cur.fetchall()
     v_no = records_v[0][0]
     return v_no

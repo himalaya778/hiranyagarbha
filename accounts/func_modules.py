9b0d75966d296cc43945080baaabe6d2166464e1
unknown <himalya.kxa@gmail.com>
2018-12-24 22:52:56 +0530

    smoid

--- accounts/func_modules.py
+++ accounts/func_modules.py
@@ -1,19 +1,1 @@
-from .views import conn
-cur = conn.cursor()
-def village_list(user_name):
-    villages = []
-    cur.execute("SELECT smo_id FROM smo_level WHERE smo = %s", (user_name,))
-    rec_smo = cur.fetchall()
-    smo_id = rec_smo[0][0]
 
-    cur.execute("SELECT anm_id FROM anm_level WHERE smo_id = %s",(smo_id,))
-    rec_anm = cur.fetchall()
-    anm_ids = rec_anm[0]
-    if(len(anm_ids)>0):
-        for a in anm_ids:
-            cur.execute("SELECT village FROM village_level WHERE anm_id = %s", (a,))
-            rec_village = cur.fetchall()
-            vill = rec_village[0][0]
-            villages.append(vill)
-
-    return villages

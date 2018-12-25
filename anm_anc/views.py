6b074bc829afd4394c89cc99e0ff21de795c78b3
unknown <himalya.kxa@gmail.com>
2018-12-25 23:33:23 +0530

    alb

--- anm_anc/views.py
+++ anm_anc/views.py
@@ -375,7 +375,7 @@ def anc_visit(request):
         #             tobacohol, preg_disease, bleeding_check, iugr, alb,
         #             hrisk_check, c_f, v_f, h_f, anm_id))
 
-        cur.execute("""UPDATE anm_anc SET weight=%s::INTEGER[] ,bp_1=%s::INTEGER[] ,bp_2=%s::INTEGER[] ,malrepresentation=%s::TEXT[] ,gdm=%s::INTEGER[] ,anemia=%s::TEXT[] ,
+        cur.execute("""UPDATE anm_anc SET weight=array_append(weight, 60) ,bp_1=%s::INTEGER[] ,bp_2=%s::INTEGER[] ,malrepresentation=%s::TEXT[] ,gdm=%s::INTEGER[] ,anemia=%s::TEXT[] ,
                 haemoglobin=%s::INTEGER[] ,thyroid=%s::TEXT[] , alcohol_tobacco_check=%s::BOOLEAN[] ,preg_related_disease=%s::BOOLEAN[] ,bleeding_check=%s::BOOLEAN[] ,iugr=%s::BOOLEAN[] ,
 
                 constant_factors=%s::TEXT[] , variable_factors=%s::TEXT[] ,hrisk_factors=%s::TEXT[] WHERE patient_id = %s""",

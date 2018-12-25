a2eda6f4ceda563966a540e7a5e87c7f3520446c
unknown <himalya.kxa@gmail.com>
2018-12-25 23:38:50 +0530

    alb

--- anm_anc/views.py
+++ anm_anc/views.py
@@ -375,11 +375,11 @@ def anc_visit(request):
         #             tobacohol, preg_disease, bleeding_check, iugr, alb,
         #             hrisk_check, c_f, v_f, h_f, anm_id))
 
-        cur.execute("""UPDATE anm_anc SET weight=array_append(weight, 60) ,bp_1=%s::INTEGER[] ,bp_2=%s::INTEGER[] ,malrepresentation=%s::TEXT[] ,gdm=%s::INTEGER[] ,anemia=%s::TEXT[] ,
+        cur.execute("""UPDATE anm_anc SET weight=array_cat(weight, %s) ,bp_1=%s::INTEGER[] ,bp_2=%s::INTEGER[] ,malrepresentation=%s::TEXT[] ,gdm=%s::INTEGER[] ,anemia=%s::TEXT[] ,
                 haemoglobin=%s::INTEGER[] ,thyroid=%s::TEXT[] , alcohol_tobacco_check=%s::BOOLEAN[] ,preg_related_disease=%s::BOOLEAN[] ,bleeding_check=%s::BOOLEAN[] ,iugr=%s::BOOLEAN[] ,
 
                 constant_factors=%s::TEXT[] , variable_factors=%s::TEXT[] ,hrisk_factors=%s::TEXT[] WHERE patient_id = %s""",
-                    ( bp1, bp2, malrep, gdm, anemia, hb, thyroid,
+                    (weight, bp1, bp2, malrep, gdm, anemia, hb, thyroid,
                      tobacohol, preg_disease, bleeding_check, iugr,
                      c_f, v_f, h_f, p_id,))
 

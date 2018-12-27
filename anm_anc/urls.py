9435d3923970743480ce020de51aa294595e4307
unknown <himalya.kxa@gmail.com>
2018-12-27 23:53:18 +0530

    text

--- anm_anc/urls.py
+++ anm_anc/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 url('patient_reg' , views.patient_registry),
 url('patient_anc' , views.anc_visit),
-url('anm_app' , views.anm_app_data)
+url('anm_app_with_anc' , views.anm_app_data_with_anc),
+url('anm_app_without_anc' , views.anm_app_data_without_anc)
 ]

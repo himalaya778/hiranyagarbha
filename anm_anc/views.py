b1e00f06bcb62ecc2b1fb552077ad0840dad4495
unknown <himalya.kxa@gmail.com>
2018-12-29 02:33:39 +0530

    dat anc visit

--- anm_anc/views.py
+++ anm_anc/views.py
@@ -429,7 +429,7 @@ def anc_visit(request):
         bleeding_check=array_append(bleeding_check,%s),iugr=array_append(iugr,%s),      
                 constant_factors=array_append(constant_factors,%s) , variable_factors=array_append(variable_factors,%s) ,
                 hrisk_factors=array_append(hrisk_factors,%s),
-                visit_no=%s,hrisk_check=%s, anm_anc_date=array_append(anm_anc_date, %s), WHERE patient_id = %s""",
+                visit_no=%s,hrisk_check=%s, anm_anc_date=array_append(anm_anc_date, %s) WHERE patient_id = %s""",
                     ( weight,bp1, bp2,malrep, gdm, anemia, hb, thyroid,
                      tobacohol, preg_disease, bleeding_check, iugr,
                       const_factors, variable_factors, hrisk_factors, visit_number,hrisk_check,anm_anc_date,p_id,))

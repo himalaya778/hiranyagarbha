7a0217e41afdf41eab4b62158b36729e55ff1efb
unknown <himalya.kxa@gmail.com>
2018-12-28 15:22:21 +0530

    text message

--- anm_anc/views.py
+++ anm_anc/views.py
@@ -92,7 +92,7 @@ def anc_visit(request):
     #constant high risk factors check
     cur.execute("SELECT age,lmp_date,edd_date,created_at::DATE,patient_name FROM patient_level WHERE patient_id = %s" , (p_id,))
     age_rec = cur.fetchall()
-    print(len(age_rec[0]))
+    print(len(age_rec))
     print(age_rec)
     age = age_rec[0][0] #1
     lmp = age_rec[0][1]
